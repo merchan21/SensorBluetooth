78e4ce80de139b8120d5559cf607bfc76f30c39d
merchan21 <ronaldmerchan10@gmail.com>
2021-10-13 10:51:33 -0500

    Segundo Commit

---
+++ app/src/main/java/com/vana/blue/InicioActivity.java
@@ -0,0 +1,29 @@
+package com.vana.blue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class InicioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio);
+
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(InicioActivity.this, PrincipalActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        Timer tiempo = new Timer();
+        tiempo.schedule(tarea, 5000);
+    }
+}
---
+++ app/src/main/java/com/vana/blue/PrincipalActivity.java
@@ -0,0 +1,29 @@
+package com.vana.blue;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class PrincipalActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+        button = findViewById(R.id.CAMBIAR);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(PrincipalActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
